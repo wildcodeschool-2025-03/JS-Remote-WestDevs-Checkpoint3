ccc2870d1024a679b9a8f03ab39e1495af9f248e
ahmed firoum <ahmedfiroumk@gmail.com>
2025-06-30 11:03:42 +0200

    step 4 done

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,5 +1,4 @@
 import type { RequestHandler } from "express";
-import databaseClient from "../../../database/client";
 import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
@@ -14,13 +13,29 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const validate: RequestHandler = async (req, res, next) => {
   // your code here
-  // const { coord_x, coord_y } = req.body;
-  // const coordinate = await tileRepository.readByCoordinates(coord_x, coord_y);
-  // if (!coord_x || coord_y) {
-  //   res.sendStatus(422);
-  // } else {
-  //   next();
-  // }
+  try {
+    const { coord_x, coord_y } = req.body;
+    if (coord_x === undefined || coord_y === undefined) {
+      res.sendStatus(422);
+      return;
+    }
+    if (coord_x < 0 || coord_x > 11) {
+      res.sendStatus(422);
+      return;
+    }
+    if (coord_y < 0 || coord_y > 5) {
+      res.sendStatus(422);
+      return;
+    }
+    const coordinate = await tileRepository.readByCoordinates(coord_x, coord_y);
+    if (!coordinate) {
+      res.sendStatus(422);
+      return;
+    }
+    next();
+  } catch (err) {
+    res.sendStatus(500);
+  }
 };
 
 export default {
