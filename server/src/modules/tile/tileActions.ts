0d67c0e96d4814598565ca9c41e8d30efbea6a3e
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2025-06-30 10:45:47 +0200

    Step 4 complete

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -11,7 +11,19 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+
+    const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+    if (tile.length === 0) {
+      res.sendStatus(422);
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
