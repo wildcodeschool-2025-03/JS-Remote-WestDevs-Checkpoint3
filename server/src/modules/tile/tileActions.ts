27350b5cdd14f374157d85f3a0e5981ebb85a6fd
B-Maxp <max.bpr@outlook.fr>
2025-06-30 12:29:02 +0200

    :zap: Step 4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -12,7 +12,22 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+
+    const coordinates = await tileRepository.readByCoordinates(
+      coord_x,
+      coord_y,
+    );
+
+    if (coordinates.length === 0) {
+      res.sendStatus(422);
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
