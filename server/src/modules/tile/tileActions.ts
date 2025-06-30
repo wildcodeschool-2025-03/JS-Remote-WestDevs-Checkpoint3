de74e2eaef0d773b13d80eaf3d9d43351684c569
tomabadie <abadie.thomas@gmail.com>
2025-06-30 12:32:32 +0200

    Done step4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -13,6 +13,23 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const validate: RequestHandler = async (req, res, next) => {
   // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+
+    if (coord_x === undefined || coord_y === undefined) {
+      res.sendStatus(422);
+    } else {
+      const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+      if (tile.length === 0) {
+        res.sendStatus(422);
+      } else {
+        next();
+      }
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
