c50064b7d556636ee00787c44fb31faf69606ae5
Romain-dev85 <r.auvinet@gmail.com>
2025-06-30 12:29:38 +0200

    unfinished step 4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -8,7 +8,19 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+
+    if (!coord_x || !coord_y) {
+      res.sendStatus(422);
+    }
+
+    if (coord_x > 0 || coord_x <= 11 || coord_y > 0 || coord_y <= 5) {
+      next();
+    } else {
+      res.sendStatus(422);
+    }
+  } catch (err) {}
 };
 
 export default {
