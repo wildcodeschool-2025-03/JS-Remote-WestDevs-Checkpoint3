25962303f18c0bb725d31e16155a712d579996be
Leïla <leila-dahmani@live.fr>
2025-06-30 10:59:18 +0200

    test 2

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,16 +1,14 @@
 import express from "express";
-
+import boatActions from "./modules/boat/boatActions";
+import gameActions from "./modules/game/gameActions";
+import tileActions from "./modules/tile/tileActions";
 const router = express.Router();
 
 /* ************************************************************************* */
-// Define Your API Routes Here
+router.get("/api/tiles", tileActions.browse);
 /* ************************************************************************* */
 
-import boatActions from "./modules/boat/boatActions";
-
 router.get("/api/boats", boatActions.browse);
-
-import gameActions from "./modules/game/gameActions";
 
 router.post("/api/games", gameActions.add);
 
