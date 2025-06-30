9b507412ad8836db0b75ebfd4494890db664def1
ahmed firoum <ahmedfiroumk@gmail.com>
2025-06-30 11:38:10 +0200

    refacto

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -15,24 +15,19 @@ const validate: RequestHandler = async (req, res, next) => {
   // your code here
   try {
     const { coord_x, coord_y } = req.body;
-    if (coord_x === undefined || coord_y === undefined) {
+    if (coord_x < 0 || coord_x > 11 || coord_y < 0 || coord_y > 5) {
       res.sendStatus(422);
-      return;
+    } else {
+      const coordinate = await tileRepository.readByCoordinates(
+        coord_x,
+        coord_y,
+      );
+      if (!coordinate) {
+        res.sendStatus(422);
+      } else {
+        next();
+      }
     }
-    if (coord_x < 0 || coord_x > 11) {
-      res.sendStatus(422);
-      return;
-    }
-    if (coord_y < 0 || coord_y > 5) {
-      res.sendStatus(422);
-      return;
-    }
-    const coordinate = await tileRepository.readByCoordinates(coord_x, coord_y);
-    if (!coordinate) {
-      res.sendStatus(422);
-      return;
-    }
-    next();
   } catch (err) {
     res.sendStatus(500);
   }
