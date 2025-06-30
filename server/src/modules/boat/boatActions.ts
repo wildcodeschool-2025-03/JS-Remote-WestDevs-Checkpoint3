e765680ab1235d78364e4e018e0ac9d84c281e08
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2025-06-30 10:12:55 +0200

    Step 3 complete

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -16,7 +16,19 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const id = req.params.id;
+    const boat = { ...req.body, id };
+
+    const affectedRows = await boatRepository.update(boat);
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
