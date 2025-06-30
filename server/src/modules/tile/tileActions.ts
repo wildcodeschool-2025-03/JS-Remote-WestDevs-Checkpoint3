0e39e60f77a1c80773fc1086efd12f81a087615c
Estelle Michard <michardestelleem@gmail.com>
2025-06-30 10:40:10 +0200

    Commit Step2 done

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,11 +1,24 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const tile = await tileRepository.readAll();
+    res.json(tile);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { type, has_treasure } = req.body;
+    if (!type || !has_treasure) {
+      res.status(403).json("The entries are not valid");
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
--- server/src/router.ts
+++ server/src/router.ts
@@ -9,8 +9,10 @@ const router = express.Router();
 import boatActions from "./modules/boat/boatActions";
 
 router.get("/api/boats", boatActions.browse);
+router.get("/api/tiles", tileActions.browse);
 
 import gameActions from "./modules/game/gameActions";
+import tileActions from "./modules/tile/tileActions";
 
 router.post("/api/games", gameActions.add);
 
